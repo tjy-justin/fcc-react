3918557d812e23a46617f90a0c3b78e5f487513a
Justin Tan <raineedust@proton.me>
2023-09-25 20:06:08 +0800

    Topic: JSX JS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
+// setup vars
+
+const title = "Code Red";
+const author = "Kyle Mills";
+const img =
+	"https://storage.googleapis.com/du-prd/books/images/9781982164997.jpg";
+
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book />
-			<Book />
 			<Book />
 		</section>
 	);
@@ -16,29 +21,21 @@ function BookList() {
 const Book = () => {
 	return (
 		<article className="book">
-			<Image />
-			<Title />
-			<Author />
+			<img src={img} alt="" />
+			<h1>{title}</h1>
+			<h4>{author.toUpperCase()}</h4>
 		</article>
 	);
 };
 
 // uses parentheses over curly braces if no return statement included.
 
-const Image = () => (
-	<img
-		src="https://storage.googleapis.com/du-prd/books/images/9781982164997.jpg"
-		alt=""
-	/>
-);
-
-const Title = () => <h1>Code Red</h1>;
-
-const Author = () => (
-	<h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-		Kyle Mills
-	</h4>
-);
+// const Image = () => (
+// 	<img
+// 		src="https://storage.googleapis.com/du-prd/books/images/9781982164997.jpg"
+// 		alt=""
+// 	/>
+// );
 
 // const Greeting = () => {
 // 	return React.createElement(
